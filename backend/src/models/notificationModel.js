bee73c924d0f7dc8cdbd16cb346794f3ef1405fc
kuma <kumaleta2021@gmail.com>
2025-02-19 11:04:21 +0300

    notification model added

---
+++ backend/src/models/notificationModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema({
+  recipient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  }, // User receiving the notification
+  message: { type: String, required: true }, // Notification message
+  type: {
+    type: String,
+    enum: ["donation", "application", "need", "payment", "general"],
+    required: true,
+  }, // Type of notification
+  seen: { type: Boolean, default: false }, // Whether the notification has been seen
+  createdAt: { type: Date, default: Date.now }, // Timestamp
+});
+
+module.exports = mongoose.model("Notification", notificationSchema);
