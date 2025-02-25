bb0c554b08efb0d60d5bdf6f498abde859054465
Ermias Wakgari Yadeta <118937954+ERMIASWAKGARI@users.noreply.github.com>
2025-02-25 10:41:01 +0300

    added filter,search, sort,limit and paginate for user management

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -2,19 +2,45 @@ const User = require("../models/User");
 const asyncWrapper = require("../middleware/asyncWrapper");
 const AppError = require("../utils/appError");
 const sendSuccessResponse = require("../utils/responseHelper");
+const APIFeatures = require("../utils/apiFeatures");
 
 // Get all users
 const getAllUsers = asyncWrapper(async (req, res) => {
-  const users = await User.find().select("-password");
+  // Apply filtering, sorting, pagination, and search
+  // console.log(req.query);
+  const features = new APIFeatures(User.find(), req.query)
+    .filter()
+    .search()
+    .sort()
+    .limit()
+    .paginate();
+  // console.log(features);
+
+  const users = await features.executeQuery(); // Execute the query
+  // console.log(users);
+  const totalUsers = users.length;
+  if (!totalUsers) {
+    throw new AppError("No users found", 404);
+  }
+
   sendSuccessResponse(res, 200, "Users retrieved successfully", {
-    totalUsers: users.length,
+    totalUsers,
     users,
   });
 });
 
 // Get user by ID
 const getUserById = asyncWrapper(async (req, res) => {
-  const user = await User.findById(req.params.id).select("-password");
+  const features = new APIFeatures(User.findById(req.params.id), req.query)
+    .filter()
+    .search()
+    .sort()
+    .limit()
+    .paginate();
+
+  const user = await features.executeQuery();
+
+  // const user = await User.findById(req.params.id).select("-password");
   if (!user) {
     throw new AppError("User not found", 404);
   }
