40a9e71628b9b052444f32a4a3ed7984e1addd19
murtessa <murtessajabesa65@gmail.com>
2025-03-26 22:38:14 +0300

    ready for everyone

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,18 @@
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { UserProvider } from './context/UserContext'; // Import the provider
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { UserProvider } from "./context/UserContext"; // Import the provider
 
-import App from './App.jsx';
-import './index.css';
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <UserProvider>
       <GoogleOAuthProvider clientId="1021431617656-ngngp6r2q4q2b53f12vio2pjq85hjsgj.apps.googleusercontent.com">
         <App />
-      </GoogleOAuthProvider>{' '}
+      </GoogleOAuthProvider>{" "}
     </UserProvider>
   </StrictMode>
 );
