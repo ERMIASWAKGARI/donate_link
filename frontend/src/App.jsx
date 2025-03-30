6694656b2b47926a38b90fbfdcc45ade4bdfc8ba
Biniam batu <Biniambatu12@gmail.com>
2025-03-30 18:53:41 +0300

    login page and register page  modification

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,6 @@ import Footer from "./components/common/Footer";
 import Header from "./components/common/Header";
 import ScrollToTop from './components/common/ScrollToTop'
 import AllRoutes from "./routes/AllRoutes";
-import Header_for_indDonor from '../src/pages/Donor/IndividualDonor/Header_for_indDonor'
-import Header_for_Organizationdonor from "./pages/Donor/OrganizationalDonor/Header_for_Organizationdonor"
 
 function App() {
   return (
